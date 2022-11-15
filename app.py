ae0b7febaf39b84a19340c5f10c0bd225bfaa16f
ADI SURYA BASRI <54743290+adisurya1997@users.noreply.github.com>
2022-11-15 17:22:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,22 +27,6 @@ def metricscpu():
     # print(response.status_code)
     return response.json()
 
-# @app.get("/dashboard/metrics/avgcpu")
-# def metricsavgcpu():
-#     d=str(datetime.datetime.now())
-#     p='%Y-%m-%d %H:%M:%S.%f'
-#     a = int(time.mktime(time.strptime(d,p)))
-#     e = int(time.mktime(time.strptime(d,p))) - 3600
-#     z = str(a)
-#     x = str(e)
-#     url="http://10.10.65.1:8080/api/v1/clusters/sapujagad?fields=metrics/cpu/Nice._avg["+x+","+z+",15],metrics/cpu/System._avg["+x+","+z+",15],metrics/cpu/User._avg["+x+","+z+",15],metrics/cpu/Idle._avg["+x+","+z+",15]&_="+z+""
-#     username = "sapujagad"
-#     password = "kayangan"
-#     response = requests.get(url, auth=(username, password))
-#     # print(response.status_code)
-#     return response.json()
-
-
 @app.get("/dashboard/metrics/avgcpu")
 def metricsavgcpu():
     d=str(datetime.datetime.now())
@@ -55,16 +39,32 @@ def metricsavgcpu():
     username = "sapujagad"
     password = "kayangan"
     response = requests.get(url, auth=(username, password))
-    test1 = str(response.json())
-    test2 = test1.replace("[", "{" )
-    test3 = test2.replace("]", "}")
-    # test = json.loads(test3)
-    test4 = test3.replace("'", '"')  
-    # test4 = test3a[:-1]
-    # data = json.loads(test4)
-#     data = json.loads(test4)
-#     return jsonify(data)
-    return (test4)
+    # print(response.status_code)
+    return response.json()
+
+
+# @app.get("/dashboard/metrics/avgcpu")
+# def metricsavgcpu():
+#     d=str(datetime.datetime.now())
+#     p='%Y-%m-%d %H:%M:%S.%f'
+#     a = int(time.mktime(time.strptime(d,p)))
+#     e = int(time.mktime(time.strptime(d,p))) - 3600
+#     z = str(a)
+#     x = str(e)
+#     url="http://10.10.65.1:8080/api/v1/clusters/sapujagad?fields=metrics/cpu/Nice._avg["+x+","+z+",15],metrics/cpu/System._avg["+x+","+z+",15],metrics/cpu/User._avg["+x+","+z+",15],metrics/cpu/Idle._avg["+x+","+z+",15]&_="+z+""
+#     username = "sapujagad"
+#     password = "kayangan"
+#     response = requests.get(url, auth=(username, password))
+#     test1 = str(response.json())
+#     test2 = test1.replace("[", "{" )
+#     test3 = test2.replace("]", "}")
+#     # test = json.loads(test3)
+#     test4 = test3.replace("'", '"')  
+#     # test4 = test3a[:-1]
+#     # data = json.loads(test4)
+# #     data = json.loads(test4)
+# #     return jsonify(data)
+#     return (test4)
     
 
 
